24a20098ef5bea607528c0c19f39acb73f529a04
Gabriel Araújo <142544309+Gabriel-SL-Araujo@users.noreply.github.com>
2024-09-13 11:03:44 -0300

    Constructor, getters and setters of Medico.java done

--- src/edu.project.clinica/Medico.java
+++ src/edu.project.clinica/Medico.java
@@ -8,6 +8,11 @@ public class Medico{
     
     Medico(String nome){
         this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.ID = ID;
+        this CPF = CPF;
+        this.especialidade = especialidade;
     }
     
     String getNome(){
@@ -16,5 +21,45 @@ public class Medico{
     
     void setNome(String nome){
         return nome;
+    }
+
+    String getTelefone(){
+        this.telefone = telefone;
+    }
+    
+    void setTelefone(String telefone){
+        return telefone;
+    }
+
+    String getEmail(){
+        this.email = email;
+    }
+    
+    void setEmail(String email){
+        return email;
+    }
+
+    ID getID(){
+        this.ID = ID;
+    }
+    
+    void setID(int ID){
+        return ID;
+    }
+
+    String getCPF(){
+        this.CPF = CPF;
+    }
+    
+    void setCPF(String CPF){
+        return CPF;
+    }
+
+    String getNome(){
+        this.nome = nome;
+    }
+    
+    void setEspecialidade(String especialidade){
+        return especialidade;
     }
 }
