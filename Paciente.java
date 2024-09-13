a4ff3649aaccd7c75907bbb81fee590939c424e2
Gabriel A. <142544309+Masterofdarkdarkness@users.noreply.github.com>
2024-09-13 01:07:28 +0000

    Variables Instances Created

--- Paciente.java
+++ Paciente.java
@@ -1,3 +1,21 @@
-public class Consulta {
-
+public class Paciente{
+    String nome;
+    String telefone;
+    String email;
+    int ID;
+    String CPF;
+    String endereco;
+    
+    Paciente(String nome, int codigo){
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+    
+    String getNome(){
+        this.nome = nome;
+    }
+    
+    void setNome(String nome){
+        return nome;
+    }
 }
