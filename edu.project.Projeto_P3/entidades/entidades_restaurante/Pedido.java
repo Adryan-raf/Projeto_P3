de2825e6898dfe1f8b257c6e329388ac7226f34a
Gabriel Araújo <142544309+Gabriel-SL-Araujo@users.noreply.github.com>
2024-11-28 22:52:58 -0300

    Update Pedido.java

--- edu.project.Projeto_P3/entidades/entidades_restaurante/Pedido.java
+++ edu.project.Projeto_P3/entidades/entidades_restaurante/Pedido.java
@@ -1,59 +1,26 @@
 package entidades.entidades_restaurante;
 
-
 import java.util.ArrayList;
 
-public class Pedido{
-    private int numeroDaMesa;
+public class Pedido {
+    private int numeroMesa;
     private ArrayList<itensDoPedido> itens;
-    
-    public Pedido(int numeroDaMesa){
-        this.numeroDaMesa = numeroDaMesa;
+
+    public Pedido(int numeroMesa) {
+        this.numeroMesa = numeroMesa;
         this.itens = new ArrayList<>();
     }
-    
-    public int getNumeroDaMesa(){
-        return numeroDaMesa; 
-    }
-    
-    public void adicionarItem(itensDoPedido item){
+
+    public void adicionarItem(itensDoPedido item) {
         itens.add(item);
     }
-    
-    public double calcularTotal(){
-        double totalidade = 0;
-        for(itensDoPedido item : itens){
-            totalidade += item.calcularTotal();
-        }
-        return totalidade;
+
+    public void limparPedido() {
+        itens.clear();
+        System.out.println("Pedido limpo.");
     }
-    
-    public void verPedido(){
-        if (estaVazio() == true) {
-            System.out.println("O pedido da mesa " + numeroDaMesa + " está vazio.\n");
-        } else {
-            System.out.println("Pedido da mesa " + numeroDaMesa + ":");
-            for (itensDoPedido item : itens) {
-                System.out.println(item);
-            }
-        }
+
+    public double calcularTotal() {
+        return itens.stream().mapToDouble(itensDoPedido::calcularTotal).sum();
     }
-    
-    public void removerItem(int index) {
-        if (index >= 0 && index < itens.size()) {
-            itens.remove(index);
-            System.out.println("Item removido da mesa " + numeroDaMesa + ".");
-        } else {
-            System.out.println("Erro: Índice inválido para remoção.");
-        }
-    }
-    
-    public ArrayList<itensDoPedido> getItens() {
-            return itens;
-    }
-    
-    public boolean estaVazio(){
-        return itens.isEmpty();
-    }
-    
 }
