3911e6640afff5d9361ded481e5804e018e78495
Gabriel Araújo <142544309+Gabriel-SL-Araujo@users.noreply.github.com>
2024-11-28 22:52:27 -0300

    Update Mesa.java

--- edu.project.Projeto_P3/entidades/entidades_restaurante/Mesa.java
+++ edu.project.Projeto_P3/entidades/entidades_restaurante/Mesa.java
@@ -1,72 +1,49 @@
 package entidades.entidades_restaurante;
 
-public class Mesa{
+public class Mesa {
     private int numero;
     private boolean disponibilidade;
     private Pedido pedido;
-    
-    public Mesa(int numero){
+
+    public Mesa(int numero) {
         this.numero = numero;
         this.disponibilidade = true;
     }
-    
-    
-    public boolean estaOcupada(){
+
+    public boolean estaOcupada() {
         return !disponibilidade;
     }
-    
-    public void ocuparMesa(){
-        if(disponibilidade == true){
-            this.disponibilidade = false;
-            this.pedido = new Pedido(numero);
-            System.out.println("Mesa " + numero + " acaba de ser ocupada.\n");
+
+    public void ocuparMesa() {
+        if (disponibilidade) {
+            disponibilidade = false;
+            pedido = new Pedido(numero);
+            System.out.println("Mesa " + numero + " ocupada.");
         } else {
-            System.out.println("Erro: Mesa " + numero + " já está ocupada.\n");
+            System.out.println("Mesa já está ocupada.");
         }
-    }
-    
-    public void liberarMesa(){
-        if(disponibilidade == false){
-            this.disponibilidade = true;
-            this.pedido = null;
-            System.out.println("Mesa " + numero + " acaba de ser liberada.\n");
-        } else{
-            System.out.println("Erro: Mesa " + numero + " ja está desocupada.\n");
-        }
-    }
-    
-     public void adicionarItemAoPedido(itensDoPedido item) {
-        if(disponibilidade == false && pedido != null) {
-            pedido.adicionarItem(item);
-            System.out.println("Item adicionado: " + item);
-        } else {
-            System.out.println("Erro: a mesa está livre ou não há pedido ativo!\n");
-        }
-    }
-    
-    public void removerPedido(int numero){
-        if(pedido != null){
-            pedido = null;
-        } else{
-            System.out.println("ERRO: Não há pedidos para remover.\n");
-        }
-    }
-    
-    public void removerItemDoPedido(int index) {
-        if (pedido != null) {
-            if (index >= 0 && index < pedido.getItens().size()) {
-                pedido.removerItem(index);
-                System.out.println("Item removido com sucesso.");
-            } else {
-                System.out.println("Erro: Índice inválido.");
-            }
-        } else {
-            System.out.println("Erro: Não há pedido ativo.");
-        }
-    }   
-    
-    public Pedido getPedido(){
-        return pedido;
     }
 
+    public void liberarMesa() {
+        disponibilidade = true;
+        pedido = null;
+    }
+
+    public void adicionarItemAoPedido(itensDoPedido item) {
+        if (pedido != null) {
+            pedido.adicionarItem(item);
+        }
+    }
+
+    public void removerPedido() {
+        if (pedido != null) {
+            pedido.limparPedido();
+        }
+    }
+
+    public double fecharConta() {
+        double total = pedido.calcularTotal();
+        liberarMesa();
+        return total;
+    }
 }
