14574779e0d5c3e2ee5e7f762f918edeaf8d9076
Jonas Mendes <128096948+jonas-jhz@users.noreply.github.com>
2024-12-02 23:03:05 +0000

    Comments remove

--- edu.project.Projeto_P3/edu/project/main/ClinicaMedica.java
+++ edu.project.Projeto_P3/edu/project/main/ClinicaMedica.java
@@ -11,7 +11,6 @@ public class ClinicaMedica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Criando Paciente e Medico
         Paciente paciente = new Paciente("João", "123456789", "joao@gmail.com", "senha123", "999999999", "Hipertensão", "Unimed");
         Medico medico = new Medico("Dra. Silva", "987654321", "dra.silva@gmail.com", "senha456", "888888888", "Cardiologista", "9h - 17h");
 
@@ -23,7 +22,7 @@ public class ClinicaMedica {
             System.out.println("3. Exibir Prontuário do Paciente");
             System.out.println("4. Sair");
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir o '\n' deixado pelo nextInt()
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
@@ -33,8 +32,8 @@ public class ClinicaMedica {
 
                     if (medico.disponibilidade(data)) {
                         Consulta novaConsulta = new Consulta(paciente, medico, data);
-                        medico.adicionarConsultaAgenda(novaConsulta); // Adiciona à agenda do médico
-                        paciente.adicionarConsultaProntuario(novaConsulta); // Adiciona ao prontuário do paciente
+                        medico.adicionarConsultaAgenda(novaConsulta);
+                        paciente.adicionarConsultaProntuario(novaConsulta);
                         System.out.println("Consulta marcada com sucesso!");
                     } else {
                         System.out.println("O médico já tem uma consulta nesse horário.");
@@ -42,17 +41,15 @@ public class ClinicaMedica {
                     break;
 
                 case 2:
-                    // Submenu de Ações sobre a Consulta
                     if (paciente.getProntuario().isEmpty()) {
                         System.out.println("Nenhuma consulta para realizar ações.");
                     } else {
-                        Consulta consultaAtual = paciente.getProntuario().getLast(); // Última consulta realizada
+                        Consulta consultaAtual = paciente.getProntuario().getLast();
                         exibirSubmenuConsulta(scanner, consultaAtual);
                     }
                     break;
 
                 case 3:
-                    // Exibir prontuário do paciente
                     paciente.exibirProntuario();
                     break;
 
@@ -68,7 +65,6 @@ public class ClinicaMedica {
         scanner.close();
     }
 
-    // Submenu de Ações sobre a Consulta
     public static void exibirSubmenuConsulta(Scanner scanner, Consulta consulta) {
         int opcao;
         do {
@@ -78,7 +74,7 @@ public class ClinicaMedica {
             System.out.println("3. Concluir Consulta");
             System.out.println("4. Voltar ao Menu Principal");
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir o '\n' deixado pelo nextInt()
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
